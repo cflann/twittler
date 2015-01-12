dcd382f90a64107336698b148ffbcf9b6b995596
Conor Flannigan <cflanni1@jhu.edu>
2015-01-11 18:49:43 -0800

    Can add new tweets

--- application.js
+++ application.js
@@ -8,13 +8,18 @@ $(document).ready(function() {
         var tweet = streams.home[length-i];
         var $tweet = $('<div></div>');
         $tweet.addClass('tweet');
+        
         var $at = $('<div></div>');
         var $date = $('<div></div>');
-        var $message = $('<div></div>');
+        var $message = $('<div></div>');        
         $at.addClass('tweet-at');
         $date.addClass('tweet-date');
         $message.addClass('tweet-message');
+
         $at.text('@' + tweet.user);
+        $at.on('click', function() {
+          // modal tweet list
+        });
         $date.text('Star Date: ' + tweet.created_at);
         $message.text(tweet.message);
         $tweet.append($at, $date, $message);
@@ -36,5 +41,49 @@ $(document).ready(function() {
   };
 
   showNewest();
+
+  var tweetDiv = function(tweet) {
+    var $tweet = $('<div></div>');
+    $tweet.addClass('tweet');
+    
+    var $at = $('<div></div>');
+    var $date = $('<div></div>');
+    var $message = $('<div></div>');        
+    $at.addClass('tweet-at');
+    $date.addClass('tweet-date');
+    $message.addClass('tweet-message');
+
+    $at.text('@' + tweet.user);
+    $at.on('click', function() {
+      // modal tweet list
+    });
+    $date.text('Star Date: ' + tweet.created_at);
+    $message.text(tweet.message);
+    $tweet.append($at, $date, $message);
+    return $tweet;
+  };
+
+  var $addTweet = function(tweet) {
+    var $tweet = tweetDiv(tweet);
+    $tweet.css('display', 'none');
+    $('#new-tweet').after($tweet);
+    $tweet.slideDown();
+    
+    var $tweets = $('.tweet');
+    if ($tweets.length > 20) {
+      $tweets.last().remove();
+    }
+  };
+
+  window.visitor = "visitor";
+  streams.users.visitor = [];
+  
+  $('#new-tweet').on('click', 'button', function() {
+    var $input = $('#new-tweet').find('input');
+    var message = $input.val();
+    writeTweet(message);
+    $addTweet(streams.home[streams.home.length-1]);
+    $input.val('');    
+  });
   
 });
