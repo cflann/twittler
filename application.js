a45569fd9c08f0f6a68d7288fa688ac550e7dbac
Conor Flannigan <cflanni1@jhu.edu>
2015-01-11 17:08:11 -0800

    List newest tweets

---
+++ application.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  var getNewest = function() {
+    var newest = $('<div></div>').addClass('newest');
+    var length = streams.home.length;
+    for (var i = 1; i < 11; i++) {
+      if (length - i > -1) {
+        var tweet = streams.home[length-i];
+        var $tweet = $('<div></div>');
+        $tweet.addClass('tweet');
+        var $at = $('<div></div>');
+        var $date = $('<div></div>');
+        var $message = $('<div></div>');
+        $at.addClass('tweet-at');
+        $date.addClass('tweet-date');
+        $message.addClass('tweet-message');
+        $at.text('@' + tweet.user);
+        $date.text('Star Date: ' + tweet.created_at);
+        $message.text(tweet.message);
+        $tweet.append($at, $date, $message);
+        newest.append($tweet);
+      }
+    }
+    return newest;
+  };
+
+  var showNewest = function() {
+    if ($('.newest').length > 0) {
+      $('.newest').remove();
+    }
+
+    $('main').append(getNewest);
+  };
+
+  showNewest();
+  
+});
