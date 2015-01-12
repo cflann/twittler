1fa4166f8afbc58d0d5cfd58a55a28376307c610
Conor Flannigan <cflanni1@jhu.edu>
2015-01-11 19:42:04 -0800

    Style navbar

--- application.js
+++ application.js
@@ -1,47 +1,5 @@
 $(document).ready(function() {
 
-  var getNewest = function() {
-    var newest = $('<div></div>').addClass('newest');
-    var length = streams.home.length;
-    for (var i = 1; i < 11; i++) {
-      if (length - i > -1) {
-        var tweet = streams.home[length-i];
-        var $tweet = $('<div></div>');
-        $tweet.addClass('tweet');
-        
-        var $at = $('<div></div>');
-        var $date = $('<div></div>');
-        var $message = $('<div></div>');        
-        $at.addClass('tweet-at');
-        $date.addClass('tweet-date');
-        $message.addClass('tweet-message');
-
-        $at.text('@' + tweet.user);
-        $at.on('click', function() {
-          // modal tweet list
-        });
-        $date.text('Star Date: ' + tweet.created_at);
-        $message.text(tweet.message);
-        $tweet.append($at, $date, $message);
-        newest.append($tweet);
-      }
-    }
-    return newest;
-  };
-
-  var showNewest = function() {
-    if ($('.newest').length > 0) {
-      $('.newest').remove();
-    }
-
-    var $newest = getNewest();
-    $newest.css({"display": "none"});
-    $('main').append($newest);
-    $('.newest').slideDown();
-  };
-
-  showNewest();
-
   var tweetDiv = function(tweet) {
     var $tweet = $('<div></div>');
     $tweet.addClass('tweet');
@@ -62,6 +20,32 @@ $(document).ready(function() {
     $tweet.append($at, $date, $message);
     return $tweet;
   };
+  
+  var getNewest = function() {
+    var newest = $('<div></div>').addClass('newest');
+    var length = streams.home.length;
+    for (var i = 1; i < 11; i++) {
+      if (length - i > -1) {
+        var tweet = streams.home[length-i];
+        var $tweet = tweetDiv(tweet);
+        newest.append($tweet);
+      }
+    }
+    return newest;
+  };
+
+  var showNewest = function() {
+    if ($('.newest').length > 0) {
+      $('.newest').remove();
+    }
+
+    var $newest = getNewest();
+    $newest.css({"display": "none"});
+    $('main').append($newest);
+    $('.newest').slideDown();
+  };
+
+  showNewest();
 
   var $addTweet = function(tweet) {
     var $tweet = tweetDiv(tweet);
